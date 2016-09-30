cbc09249cfc6edc4a2e5dde8707b48c49f0d64ed
ChienHsin-Chan <hcchien@gmail.com>
2016-09-30 13:18:42 +0800

    remove some images

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,17 +11,9 @@ export const SITE_NAME = {
 export const SITE_META = {
   DESC: '鏡傳媒以台灣為基地，是一跨平台綜合媒體，包含《鏡週刊》以及下設五大分眾內容的《鏡傳媒》網站，刊載時事、財經、人物、國際、文化、娛樂、美食旅遊、精品鐘錶等深入報導及影音內容。我們以「鏡」為名，務求反映事實、時代與人性。',
   URL: 'https://www.mirrormedia.mg/',
-  LOGO: 'https://www.twreporter.org/asset/logo.png',
+  LOGO: 'https://www.mirrormedia.mg/asset/logo.png',
   KEYWORDS: '鏡傳媒,mirror media,新聞,人物,調查報導,娛樂,美食,旅遊,精品,動漫,網路趨勢,趨勢,國際,兩岸,政治,明星,文學,劇本,新詩,散文,小說'
 }
-
-export const SECTION_NAME = [
-  { 'title': '漫熱遊', 'name': 'popular' },
-  { 'title': '新聞/人物', 'name': 'news' },
-  { 'title': '美食/旅遊', 'name': 'foodtravel' },
-  { 'title': '鏡鐘錶', 'name': 'fashion' },
-  { 'title': '娛樂', 'name': 'entertainment' }
-]
 
 export const LINK_PREFIX = {
   ARTICLE: '/article/',
@@ -51,9 +43,9 @@ export const authorTypes = {
 
 export const copyrightTypes = {
   default: {
-    string: '除另有註明，網站內容皆採用創用CC姓名標示-非商業性-禁止改作授權條款',
-    link: 'http://creativecommons.org/licenses/by-nc-nd/3.0/tw/',
-    image: 'LOGO_CC'
+    string: 'Copyright © 2015-2016 鏡傳媒',
+    link: null,
+    image: null
   },
   copyrighted: {
     string: 'Copyright © 2015-2016 鏡傳媒',
