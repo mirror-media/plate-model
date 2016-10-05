8a030bbde070027612a09bb7e8334939cb24a05b
Chaowen Chang <imkojima@me.com>
2016-10-05 12:31:15 +0800

    remove unnecessary script

--- server/server.js
+++ server/server.js
@@ -204,28 +204,7 @@ server.get('*', async function (req, res) {
                 <div id="root">${html}</div>
                 <!-- Load Intl Polyfill -->
                 <script async src="https://cdn.polyfill.io/v2/polyfill.min.js?features=Intl.~locale.zh-Hant-TW"></script>
-                <script text="text/javascript" charset="utf-8">
-                  (function() {
-                    var cx = '008042460408727773288:mvn-lce5wvo';
-                    var gcse = document.createElement('script');
-                    gcse.type = 'text/javascript';
-                    gcse.async = true;
-                    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-                    '//cse.google.com/cse.js?cx=' + cx;
-                    var s = document.getElementsByTagName('script')[0];
-                    s.parentNode.insertBefore(gcse, s);
-                    })();
-                  </script>
-                  <!-- Google Tag Manager -->
-                  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NB59ZP"
-                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','GTM-NB59ZP');</script>
-                  <!-- End Google Tag Manager -->
-                </script>
+
                 <script type="text/javascript" charset="utf-8">
                   window.__REDUX_STATE__ = '${reduxState}';
                 </script>
