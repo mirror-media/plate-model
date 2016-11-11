22a1cf8f4f6c795497c16ca9317002e33b2e6140
Chaowen Chang <imkojima@me.com>
2016-11-11 14:59:57 +0800

    Implement DFP AD

--- src/components/LatestSections.js
+++ src/components/LatestSections.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import { imageComposer } from '../utils/index'
 import { camelize } from 'humps'
+<<<<<<< 00a022ef6e2220dbb1d0cd0c226eeaf42538964d
 import ga from 'react-ga'
+=======
+import { AdSlot } from 'react-dfp'
+import { DFPID } from '../constants/index'
+>>>>>>> Implement DFP AD
 
 if (process.env.BROWSER) {
   require('./LatestSections.css')
@@ -80,7 +85,28 @@ export default class LatestSections extends Component {
           <div className="ui column" style={{ backgroundColor: 'rgba(0, 77, 162, 0.1)', marginTop: '-10px', marginBottom: '20px' }}>
             <a href="https://goo.gl/yqJaVY"  target="_blank">
               <div className="sectionBlock" style={{ marginTop: '10px', marginBottom: '10px' }}>
-                <div className="sectionImg AD" style={{ background: 'url(/asset/ads/subscribe1130.jpg) no-repeat center center', backgroundSize: 'cover', width: '300px', height: '250px' }}></div>
+                <AdSlot sizes={ [ [ 300, 250 ] ] }
+                  dfpNetworkId={DFPID}
+                  slotId={ 'mm_pc_hp_300x250_1st' }
+                  adUnit={ 'mm_pc_hp_300x250_1st' } 
+                  sizeMapping={
+                    [ 
+                      { viewport: [   0,   0 ], sizes: [ ] },
+                      { viewport: [ 970, 200 ], sizes: [ [ 970, 90 ], [ 970, 250 ], [ 300, 250 ] ]  }
+                    ] 
+                  }
+                />
+                <AdSlot sizes={ [ [ 300, 250 ] ] }
+                  dfpNetworkId={DFPID}
+                  slotId={ 'mm_mobile_hp_300x250_1st' }
+                  adUnit={ 'mm_mobile_hp_300x250_1st' } 
+                  sizeMapping={
+                    [ 
+                      { viewport: [   1,   1 ], sizes: [ [ 300, 250 ] ] },
+                      { viewport: [ 970, 200 ], sizes: [ ]  }
+                    ] 
+                  }
+                />
               </div>
             </a>
             <ul className="sectionList">
