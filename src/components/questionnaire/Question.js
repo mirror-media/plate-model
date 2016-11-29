df0c2eb132f5884a9a0ffcdcb7274f544918d5e9
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2016-11-29 12:39:55 +0800

    get rid of some marks

--- src/components/questionnaire/Question.js
+++ src/components/questionnaire/Question.js
@@ -10,21 +10,4 @@ const Question = (props) => (
   </div>
 )
 
-// class Question extends Component {
-//   constructor(props, context) {
-//     super(props, context)
-//   }
-//   render() {
-//     console.log('get the question, ', this.props)
-//     const title = _.get(this.props.question, [ 'title' ])
-//     return (
-//       <div className="quesiton">
-//         <div className="question-container">
-//           { title }
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
 export default Question
