20c2302401cfb0ddf921ac9df2ef8d3bc8a80557
Chaowen Chang <imkojima@me.com>
2016-12-01 18:00:39 +0800

    adapt Audio Box style for audio page

--- src/components/AudioList.js
+++ src/components/AudioList.js
@@ -1,10 +1,10 @@
+import More from '../components/More'
 import React, { Component } from 'react'
 import _ from 'lodash'
-import truncate from 'truncate'
-import More from '../components/More'
 
 if (process.env.BROWSER) {
   require('./LatestArticles.css')
+  require('./AudioList.css')
 }
 
 export default class AudioList extends Component {
@@ -29,35 +29,50 @@ export default class AudioList extends Component {
     const { audios, hasMore, loadMore } = this.props
 
     return  (
-      <div className="container">
+      <div className="container audioList">
         {this.renderTitle()}
         <div className="latest">
 
           { _.map(audios, (a)=>{
             //let image = imageComposer(a).mobileImage
-            let image = '/asset/review.png'
             let title = _.get(a, [ 'title' ], '')
-            let brief = ''
-            let link = '#'
+            // let brief = sanitizeHtml( _.get(a, [ 'description', 'html' ], ''), { allowedTags: [ ] })
+            
 
+            // return (
+            //   <div className="latest-block" key={a.id || a._id} >
+            //     <a href={link}>
+            //       <div className="latest-img" style={{ background: 'url('+image+') no-repeat center center', backgroundSize:'cover' }}>
+            //       </div>
+            //     </a>
+            //     <div className="latest-content">
+            //       <a href={link}>
+            //         <h2>
+            //             <span dangerouslySetInnerHTML={{ __html: title }}/><div className="cat-label"><div className="separator"></div><span>Audio</span></div>
+            //         </h2>
+            //       </a>
+            //       <div className="line">
+            //       </div>
+            //       <div className="brief">
+            //         {truncate(brief, 75)}
+            //       </div>
+            //     </div>
+            //   </div>
+            // )
             return (
-              <div className="latest-block" key={a.id || a._id} >
-                <a href={link}>
-                  <div className="latest-img" style={{ background: 'url('+image+') no-repeat center center', backgroundSize:'cover' }}>
+              <div className="audio-box" key={a.id || a._id}>
+                  <div className="audio-container ">
+                      <div className="audio-time"><span className="left">00:00</span> / 07:42</div>
+                      <div className="audio-progress">
+                          <div className="bar"></div>
+                      </div>
+                      <div className="audio-cover">
+                          <div className="audio-btn pause"></div>
+                      </div>
+                      <div className="audio-title"><span dangerouslySetInnerHTML={{ __html: title }}/></div>
+                      <div className="audio-desc"></div>
+                      <audio src="https://www.mirrormedia.mg/assets/audios/20161104173058-faca0aa139dba138532140a11957e7ef.wav"> </audio>
                   </div>
-                </a>
-                <div className="latest-content">
-                  <a href={link}>
-                    <h2>
-                        <span dangerouslySetInnerHTML={{ __html: title }}/><div className="cat-label"><div className="separator"></div><span>VideoHub</span></div>
-                    </h2>
-                  </a>
-                  <div className="line">
-                  </div>
-                  <div className="brief">
-                    {truncate(brief, 75)}
-                  </div>
-                </div>
               </div>
             )
           })}
